a428c36f63c833deeab13cbd917e75be4b66476a
gmetae <gmetae@gmetae@gmail.com>
2017-11-13 19:56:29 +0300

    Добавьте файлы проекта.

---
+++ SNMPApp/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace SNMPApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.askButton = new System.Windows.Forms.Button();
+            this.responseTextBox = new System.Windows.Forms.TextBox();
+            this.hostTextBox = new System.Windows.Forms.TextBox();
+            this.hostLabel = new System.Windows.Forms.Label();
+            this.communityTextBox = new System.Windows.Forms.TextBox();
+            this.communityLabel = new System.Windows.Forms.Label();
+            this.adapterComboBox = new System.Windows.Forms.ComboBox();
+            this.addressesListBox = new System.Windows.Forms.ListBox();
+            this.adapterLabel = new System.Windows.Forms.Label();
+            this.addressesLabel = new System.Windows.Forms.Label();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // askButton
+            // 
+            this.askButton.Location = new System.Drawing.Point(536, 25);
+            this.askButton.Name = "askButton";
+            this.askButton.Size = new System.Drawing.Size(75, 43);
+            this.askButton.TabIndex = 0;
+            this.askButton.Text = "Ask";
+            this.askButton.UseVisualStyleBackColor = true;
+            this.askButton.Click += new System.EventHandler(this.AskButtonClick);
+            // 
+            // responseTextBox
+            // 
+            this.responseTextBox.Location = new System.Drawing.Point(310, 89);
+            this.responseTextBox.Multiline = true;
+            this.responseTextBox.Name = "responseTextBox";
+            this.responseTextBox.Size = new System.Drawing.Size(301, 303);
+            this.responseTextBox.TabIndex = 1;
+            // 
+            // hostTextBox
+            // 
+            this.hostTextBox.Enabled = false;
+            this.hostTextBox.Location = new System.Drawing.Point(310, 25);
+            this.hostTextBox.Name = "hostTextBox";
+            this.hostTextBox.Size = new System.Drawing.Size(220, 20);
+            this.hostTextBox.TabIndex = 2;
+            this.hostTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.HostTextBoxKeyPress);
+            // 
+            // hostLabel
+            // 
+            this.hostLabel.AutoSize = true;
+            this.hostLabel.Enabled = false;
+            this.hostLabel.Location = new System.Drawing.Point(307, 9);
+            this.hostLabel.Name = "hostLabel";
+            this.hostLabel.Size = new System.Drawing.Size(80, 13);
+            this.hostLabel.TabIndex = 3;
+            this.hostLabel.Text = "Hostname or IP";
+            // 
+            // communityTextBox
+            // 
+            this.communityTextBox.Location = new System.Drawing.Point(400, 48);
+            this.communityTextBox.Name = "communityTextBox";
+            this.communityTextBox.Size = new System.Drawing.Size(130, 20);
+            this.communityTextBox.TabIndex = 4;
+            // 
+            // communityLabel
+            // 
+            this.communityLabel.AutoSize = true;
+            this.communityLabel.Location = new System.Drawing.Point(307, 51);
+            this.communityLabel.Name = "communityLabel";
+            this.communityLabel.Size = new System.Drawing.Size(87, 13);
+            this.communityLabel.TabIndex = 5;
+            this.communityLabel.Text = "Community name";
+            // 
+            // adapterComboBox
+            // 
+            this.adapterComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.adapterComboBox.FormattingEnabled = true;
+            this.adapterComboBox.Location = new System.Drawing.Point(12, 47);
+            this.adapterComboBox.Name = "adapterComboBox";
+            this.adapterComboBox.Size = new System.Drawing.Size(242, 21);
+            this.adapterComboBox.TabIndex = 7;
+            this.adapterComboBox.SelectedIndexChanged += new System.EventHandler(this.ComboBoxSelectedIndexChanged);
+            // 
+            // addressesListBox
+            // 
+            this.addressesListBox.FormattingEnabled = true;
+            this.addressesListBox.Location = new System.Drawing.Point(12, 115);
+            this.addressesListBox.Name = "addressesListBox";
+            this.addressesListBox.Size = new System.Drawing.Size(242, 277);
+            this.addressesListBox.TabIndex = 8;
+            this.addressesListBox.SelectedIndexChanged += new System.EventHandler(this.addressesListBox_SelectedIndexChanged);
+            // 
+            // adapterLabel
+            // 
+            this.adapterLabel.AutoSize = true;
+            this.adapterLabel.Location = new System.Drawing.Point(12, 32);
+            this.adapterLabel.Name = "adapterLabel";
+            this.adapterLabel.Size = new System.Drawing.Size(123, 13);
+            this.adapterLabel.TabIndex = 9;
+            this.adapterLabel.Text = "Choose network adapter";
+            // 
+            // addressesLabel
+            // 
+            this.addressesLabel.AutoSize = true;
+            this.addressesLabel.Location = new System.Drawing.Point(11, 99);
+            this.addressesLabel.Name = "addressesLabel";
+            this.addressesLabel.Size = new System.Drawing.Size(124, 13);
+            this.addressesLabel.TabIndex = 10;
+            this.addressesLabel.Text = "addresses in subnetwork";
+            // 
+            // checkBox
+            // 
+            this.checkBox.AutoSize = true;
+            this.checkBox.Location = new System.Drawing.Point(12, 12);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(135, 17);
+            this.checkBox.TabIndex = 11;
+            this.checkBox.Text = "Enter address manually";
+            this.checkBox.UseVisualStyleBackColor = true;
+            this.checkBox.CheckedChanged += new System.EventHandler(this.CheckBoxCheckedChanged);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(13, 79);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(161, 11);
+            this.progressBar.TabIndex = 12;
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(179, 74);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 13;
+            this.stopButton.Text = "Stop scan";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 402);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.checkBox);
+            this.Controls.Add(this.addressesLabel);
+            this.Controls.Add(this.adapterLabel);
+            this.Controls.Add(this.addressesListBox);
+            this.Controls.Add(this.adapterComboBox);
+            this.Controls.Add(this.communityLabel);
+            this.Controls.Add(this.communityTextBox);
+            this.Controls.Add(this.hostLabel);
+            this.Controls.Add(this.hostTextBox);
+            this.Controls.Add(this.responseTextBox);
+            this.Controls.Add(this.askButton);
+            this.Name = "Form1";
+            this.Text = "SNMP App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button askButton;
+        private System.Windows.Forms.TextBox responseTextBox;
+        private System.Windows.Forms.TextBox hostTextBox;
+        private System.Windows.Forms.Label hostLabel;
+        private System.Windows.Forms.TextBox communityTextBox;
+        private System.Windows.Forms.Label communityLabel;
+        private System.Windows.Forms.ComboBox adapterComboBox;
+        private System.Windows.Forms.ListBox addressesListBox;
+        private System.Windows.Forms.Label adapterLabel;
+        private System.Windows.Forms.Label addressesLabel;
+        private System.Windows.Forms.CheckBox checkBox;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button stopButton;
+    }
+}
---
+++ SNMPApp/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management;
+using System.Windows.Forms;
+using SNMPApp.Properties;
+using SnmpSharpNet;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Text;
+
+namespace SNMPApp
+{
+    public partial class Form1 : Form
+    {
+        //список валидных хостов в подсети
+        private List<string> _ipAddresses = new List<string>();
+        //флаг выполнения
+        private bool _process;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //получаем список сетевых интерфейсов
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var iface in interfaces) {  
+               //если это не туннель или петля, то добавляем интерфейс для использования
+               if ((iface.NetworkInterfaceType!= NetworkInterfaceType.Tunnel)&&(iface.NetworkInterfaceType!= NetworkInterfaceType.Loopback)) 
+                   adapterComboBox.Items.Add(iface.Description);
+            }
+        }
+
+        private void AskButtonClick(object sender, EventArgs e)
+        {
+            //к кому будем обращаться
+            string host = String.Empty;
+            if (hostTextBox.Enabled)
+                host = hostTextBox.Text;
+            if (addressesListBox.SelectedIndex != -1 && addressesListBox.Enabled)
+                    host = (string) addressesListBox.Items[addressesListBox.SelectedIndex];
+            //если не все поля заполнены, то говорим, что было б неплохо заполнить
+            if (host == String.Empty || communityTextBox.Text.Length == 0)
+            {
+                MessageBox.Show("Host or community strings are empty. Enter some values", "Error",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                return;
+            }
+            //очищаем область ответа
+            responseTextBox.Text = "";
+            string community = communityTextBox.Text;
+            //создаем запрос
+            var snmp = new SimpleSnmp(host, community);
+            //если запрос не валиден, то пишем об этом
+            if (!snmp.Valid)
+            {
+                responseTextBox.Text += Resources.Not_Valid_SNMP_HOST;
+                return;
+            }
+            //формируем тело запроса и отсылаем его
+            Dictionary<Oid, AsnType> result = snmp.Get(SnmpVersion.Ver2,
+                                                       new[]
+                                                           {
+                                                               ".1.3.6.1.2.1"/*,
+                                                               ".1.3.6.1.2.1.1.1.0", // sysDescr
+                                                               ".1.3.6.1.2.1.1.5.0", // sysName
+                                                               ".1.3.6.1.2.1.1.3.0", // upTime
+                                                               ".1.3.6.1.2.1.2.1.0", // available interfaces count
+                                                               ".1.3.6.1.2.1.1.4.0", // sysContact,
+                                                               ".1.3.6.1.2.1.1.6.0"  // sysLocation)
+                                                               */
+                                                           });
+
+            //если нет ответа, то так и скажем
+            if (result == null)
+            {
+                responseTextBox.Text += Resources.NoResults;
+                return;
+            }
+            //счетчик для форматирования
+            int i = 0;
+            foreach (var kvp in result)
+            {
+                //если есть ответ, то выводим
+                switch (i)
+                {
+                    case 1: responseTextBox.Text += "System Name:";
+                        break;
+                    case 2: responseTextBox.Text += "Uptime:";
+                        break;
+                    case 3: responseTextBox.Text += "Interfaces available:";
+                        break;
+                    case 4: responseTextBox.Text += "Contact:";
+                        break;
+                    case 5: responseTextBox.Text += "Location:";
+                        break;
+                };
+                responseTextBox.Text += kvp.Value;
+                responseTextBox.Text += "\r\n";
+                i++;
+                responseTextBox.Text += "=========================\r\n";
+            }
+            
+        }
+
+
+        //если выбали другой адаптер, то надо просканированть подсеть
+        private void ComboBoxSelectedIndexChanged(object sender, EventArgs e)
+        {
+            //начальная инициализация
+            stopButton.Enabled = true;
+            addressesListBox.Items.Clear();
+            _process = true;
+            adapterComboBox.Enabled = false;
+            string adapterIP="";
+            string adapterMask = "";
+            //получаем все интерфейсы
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var iface in interfaces)
+            {
+                //находим наш интерфейс
+                if (iface.Description == adapterComboBox.Text)
+                {
+                    //получаем свойства IP 
+                    var properties = iface.GetIPProperties();
+                    //получаем адреса
+                    var uniCast = properties.UnicastAddresses;
+                    if (uniCast!=null)
+                    {
+                        //и, наконец, вытаскиваем эти адреса
+                        foreach (UnicastIPAddressInformation uni in uniCast)
+                        {
+                            //вытаскиваем IP
+                            adapterIP = uni.Address.ToString();
+                            //вытаскиваем маску подсети
+                            if (uni.IPv4Mask == null)
+                            {
+                                adapterComboBox.Enabled = true;
+                                stopButton.Enabled = false;
+                                progressBar.Value = 0;
+                                return;
+                            }
+                            adapterMask = uni.IPv4Mask.ToString();
+                        }
+                    }
+                }
+            }
+            //эти вспомогательные переменные нужны, чтобы определить IP адреса, принадлежащие подсети
+            var IP = new byte[4];
+            var IPMask = new byte[4];
+            //разбиваем IP на октеты
+            //первый октет
+            string tmp = adapterIP;
+            tmp = tmp.Remove(tmp.IndexOf('.'));
+            IP[0] = Convert.ToByte(tmp);
+            //второй октет
+            tmp = adapterIP;
+            tmp = tmp.Remove(0, tmp.IndexOf('.') + 1);
+            tmp = tmp.Remove(tmp.IndexOf('.'));
+            IP[1] = Convert.ToByte(tmp);
+            //третий октет
+            tmp = adapterIP;
+            tmp = tmp.Remove(0, tmp.IndexOf('.') + 1);
+            tmp = tmp.Remove(0, tmp.IndexOf('.') + 1);
+            tmp = tmp.Remove(tmp.IndexOf('.'));
+            IP[2] = Convert.ToByte(tmp);
+            //четвертый октет
+            tmp = adapterIP;
+            tmp = tmp.Remove(0, tmp.IndexOf('.') + 1);
+            tmp = tmp.Remove(0, tmp.IndexOf('.') + 1);
+            tmp = tmp.Remove(0, tmp.IndexOf('.') + 1);
+            IP[3] = Convert.ToByte(tmp);
+            //разбиваем маску на октеты
+            //первый октет
+            tmp = adapterMask;
+            tmp = tmp.Remove(tmp.IndexOf('.'));
+            IPMask[0] = Convert.ToByte(tmp);
+            //второй октет
+            tmp = adapterMask;
+            tmp = tmp.Remove(0, tmp.IndexOf('.') + 1);
+            tmp = tmp.Remove(tmp.IndexOf('.'));
+            IPMask[1] = Convert.ToByte(tmp);
+            //третий октет
+            tmp = adapterMask;
+            tmp = tmp.Remove(0, tmp.IndexOf('.') + 1);
+            tmp = tmp.Remove(0, tmp.IndexOf('.') + 1);
+            tmp = tmp.Remove(tmp.IndexOf('.'));
+            IPMask[2] = Convert.ToByte(tmp);
+            //четвертый октет
+            tmp = adapterMask;
+            tmp = tmp.Remove(0, tmp.IndexOf('.') + 1);
+            tmp = tmp.Remove(0, tmp.IndexOf('.') + 1);
+            tmp = tmp.Remove(0, tmp.IndexOf('.') + 1);
+            IPMask[3] = Convert.ToByte(tmp);
+            //представляем IP в виде long числа
+            long lIP = ((IP[0] << 24) + (IP[1] << 16) + (IP[2] << 8) + IP[3]);
+            //точно так же маску
+            long lMask = ((IPMask[0] << 24) + (IPMask[1] << 16) + (IPMask[2] << 8) + IPMask[3]);
+            //вычисляем номер подсети
+            long lSubnetNum = (lIP & lMask);
+            //нижний предел подсети
+            long lStartIP = (lSubnetNum & lMask) + 1;
+            //верхний предел подсети
+            long lStopIP = (lSubnetNum | ((~lMask) - 1));
+            //текущий адес
+            var currIP = new byte[4];
+            //выставляем погресс бар
+            progressBar.Maximum = Convert.ToInt32(lStopIP - lStartIP);
+            //ищем IP адреса в сети
+            for (long i = lStartIP; i <= lStopIP; i++)
+            {
+                //если флаг выполнения сброшен, то завершаем процедуру
+                if (!_process) break;
+                //увеличиваем прогресс бар
+                progressBar.Value = Convert.ToInt32(i - lStartIP);
+                //пингуем хосты в подсети
+                var ping = new Ping();
+                PingOptions options = new PingOptions();
+                //запрещам фрагметированный пинг
+                options.DontFragment = true;
+                //информация в пинге(32байта)
+                string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+                //Создаем буфер данных 
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                //таймаут
+                int timeout = 120;
+                //текущий IP
+                currIP = BitConverter.GetBytes(i);
+                Application.DoEvents();
+                //Собираем ИП в правильном порядке байт(не в двоично-десятичной нотации)
+                IPAddress remote = new IPAddress(new byte[4]{currIP[3],currIP[2],currIP[1],currIP[0]});
+                try
+                {
+                    //пингуем
+                    PingReply reply = ping.Send(remote, timeout, buffer, options);
+                    //если ответ есть, то хост в сети
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        //добавляем хост в список
+                        string sIP = currIP[3].ToString() + "." + currIP[2].ToString() + "." + currIP[1].ToString() + "." + currIP[0].ToString();
+                        _ipAddresses.Add(sIP);
+                        addressesListBox.Items.Add(sIP);
+                        addressesListBox.Invalidate();
+                    }
+                   
+                }
+                catch (Exception) { }
+            }
+            adapterComboBox.Enabled = true;
+            stopButton.Enabled = false;
+            progressBar.Value = progressBar.Maximum;
+        }
+
+        private void CheckBoxCheckedChanged(object sender, EventArgs e)
+        {
+            adapterLabel.Enabled = !checkBox.Checked;
+            addressesLabel.Enabled = !checkBox.Checked;
+            adapterComboBox.Enabled = !checkBox.Checked;
+            addressesListBox.Enabled = !checkBox.Checked;
+            hostLabel.Enabled = checkBox.Checked;
+            hostTextBox.Enabled = checkBox.Checked;
+        }
+
+        private void HostTextBoxKeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13 )
+            {
+                AskButtonClick(this, EventArgs.Empty);
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            //сбрасываем флаг выполнения
+            _process = false;
+        }
+
+        private void addressesListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string re = addressesListBox.SelectedItem.ToString();
+            hostTextBox.Text = re;
+        }
+    }
+}
+
